57ef237c88864dcc98d0003bf978e998a1f54dcd
osama-bq <osamaqashem@gmail.com>
2026-09-15 15:21:04 +0600

    feat: update page titles and improve navigation links across multiple HTML files

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 // Navmenu toggle
 const toggleBtn = document.querySelector('.mobile-menu-toggle');
 
-toggleBtn.addEventListener('click', function() {
-    const menu = document.querySelector('.mobile-menu-list');
-    menu.classList.toggle('show');
-});
+if (toggleBtn) {
+    toggleBtn.addEventListener('click', function() {
+        const menu = document.querySelector('.mobile-menu-list');
+        menu.classList.toggle('show');
+    });
+}
 
 
 const searchBar = document.querySelector('.search-bar');
 console.log(searchBar);
-searchBar.querySelectorAll('input').forEach(input => {
+searchBar?.querySelectorAll('input').forEach(input => {
     input.addEventListener('change', event => {
         let target = event.target;
         if (target.value.trim() !== '') {
@@ -26,9 +28,11 @@ const loggedIn = true;
 if (loggedIn) {
     console.log('User is logged in');
     document.querySelector('.nav-content > .nav-buttons .btn-register').style.display = 'none';
-    document.querySelector('.nav-content > .nav-mobile-menu .btn-register').style.display = 'none';
+    if (document.querySelector('.nav-content > .nav-mobile-menu')) {
+        document.querySelector('.nav-content > .nav-mobile-menu .btn-register').style.display = 'none';
+        document.querySelector('.nav-content > .nav-mobile-menu .btn-signin').style.display = 'none';
+    }
     document.querySelector('.nav-content > .nav-buttons .btn-signin').style.display = 'none';
-    document.querySelector('.nav-content > .nav-mobile-menu .btn-signin').style.display = 'none';
     document.querySelector('.nav-content > .nav-buttons .btn-notification').style.display = 'block';
     document.querySelector('.nav-content > .nav-buttons .btn-profile').style.display = 'block';
 }
