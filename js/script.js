1828cf632c1d0119d0439de58c098170cf409d0e
osama-bq <osamaqashem@gmail.com>
2026-09-15 13:29:34 +0600

    feat: add profile-pic and notification buttons on navbar

--- js/script.js
+++ js/script.js
@@ -5,3 +5,16 @@ toggleBtn.addEventListener('click', function() {
     const menu = document.querySelector('.mobile-menu-list');
     menu.classList.toggle('show');
 });
+
+
+const loggedIn = true;
+
+if (loggedIn) {
+    console.log('User is logged in');
+    document.querySelector('.nav-content > .nav-buttons .btn-register').style.display = 'none';
+    document.querySelector('.nav-content > .nav-mobile-menu .btn-register').style.display = 'none';
+    document.querySelector('.nav-content > .nav-buttons .btn-signin').style.display = 'none';
+    document.querySelector('.nav-content > .nav-mobile-menu .btn-signin').style.display = 'none';
+    document.querySelector('.nav-content > .nav-buttons .btn-notification').style.display = 'block';
+    document.querySelector('.nav-content > .nav-buttons .btn-profile').style.display = 'block';
+}
